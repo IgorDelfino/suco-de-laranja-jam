dab2fa82c96292f156e69800f1cc8151c8a0a419
theoluiging <110366961+theoluiging@users.noreply.github.com>
2022-08-05 21:30:16 -0300

    Implementação do sistema de movimentação

---
+++ Projeto_Suco_de_Laranja/Assets/Scripts/playerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}

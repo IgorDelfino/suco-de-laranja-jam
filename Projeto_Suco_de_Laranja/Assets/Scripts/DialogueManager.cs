320b337c1de6a6be85060e2cebf7b31e28dc946c
Igor Delfino <igordelfinobarbosa56@gmail.com>
2022-08-07 20:01:12 -0300

    display de dialogo

--- Projeto_Suco_de_Laranja/Assets/Scripts/DialogueManager.cs
+++ Projeto_Suco_de_Laranja/Assets/Scripts/DialogueManager.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    public TMP_Text nameText;
+    public TMP_Text storyText;
+    public Image characterAvatar;
+
+    public Animator animator;
+
     private Queue<string> sentences;
+    private Queue<Sprite> emotions;
     private bool isDialogueHappening = false;
 
     void Start()
     {
         sentences = new Queue<string>();
+        emotions = new Queue<Sprite>();
     }
 
     public void RunDialogue(Dialogue dialogue)
@@ -30,10 +40,18 @@ public class DialogueManager : MonoBehaviour
         sentences.Clear();
 
         isDialogueHappening = true;
+        animator.SetBool("IsOnDialogue", true);
+
+        nameText.text = dialogue.nameOfObject;
 
         foreach(string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
+        }
+
+        foreach (Sprite emotion in dialogue.emotions)
+        {
+            emotions.Enqueue(emotion);
         }
     }
 
@@ -45,13 +63,28 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
+        characterAvatar.sprite = emotions.Dequeue();
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+
+        StopAllCoroutines();
+
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        storyText.text = "";
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            storyText.text += letter;
+            yield return null;
+        }
     }
 
     public void EndDialogue()
     {
         isDialogueHappening = false;
-        Debug.Log("End of story");
+        animator.SetBool("IsOnDialogue", false);
     }
 }
---
+++ Projeto_Suco_de_Laranja/Assets/Scripts/Sentence.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sentence
+{
+    public string sentence;
+
+    public Sprite emotion;
+}
